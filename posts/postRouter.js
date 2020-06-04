a45b23c2480aa72374522fdc6377820771f08a77
Natalia <nataliamirandac@hotmail.com>
2020-06-04 17:23:53 -0400

    Delete posts - is working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -30,6 +30,24 @@ router.get("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // do your magic!
+
+  const { id } = req.params;
+
+  dbPosts
+    .remove(id)
+    .then((post) => {
+      if (post && post > 0) {
+        res.status(200).json({ message: "The post has been deleted" });
+      } else {
+        res.status(404).json({ message: "The post could not be found" });
+      }
+    })
+    .catch((error) => {
+      console.log("catch error DELETE:", error);
+      res.status(500).json({
+        message: "Error removing the post",
+      });
+    });
 });
 
 router.put("/:id", (req, res) => {
