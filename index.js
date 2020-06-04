2d903ec45e892a986e1997d7b50eb39a6d868e14
Natalia <nataliamirandac@hotmail.com>
2020-06-04 16:28:15 -0400

    Server was built and is running - index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code away!
+
+const express = require("express");
+
+const server = express();
+
+//middleware
+server.use(express.json());
+
+server.listen(8000, () => {
+  console.log("\n*** Server Running on http://localhost:8000 ***\n");
+});
