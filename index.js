af77ca873718574f1484a6ea92733b7e2ae91e3f
Natalia <nataliamirandac@hotmail.com>
2020-06-04 16:54:24 -0400

    GET posts is working - Routers working - building endpoins CRUD

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // code away!
 
 const express = require("express");
-
 const server = express();
 
+//import the router
+const postRouter = require("./posts/postRouter");
+const usersRouter = require("./users/userRouter");
 //middleware
 server.use(express.json());
+
+//use routes and endpoits
+server.use("/api/posts", postRouter);
+server.use("/api/users", usersRouter);
 
 server.listen(8000, () => {
   console.log("\n*** Server Running on http://localhost:8000 ***\n");
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,20 +1,30 @@
-const express = require('express');
+const express = require("express");
+
+const dbPosts = require("./postDb");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  // do your magic!
+  dbPosts
+    .get(req.body)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      console.log("GET posts catch error:", error);
+    });
+});
+
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
